faf4fc8dfdecbc14f342b8f9987d36b8b19fdfa0
Den Kasyanov <den@den.ai>
2024-09-12 15:07:45 +0400

    Move db initialization into app lifecycle event handlers

--- server/app/main.py
+++ server/app/main.py
@@ -1,25 +1,30 @@
+import logging
 import os
 
 from fastapi import FastAPI
-from tortoise.contrib.fastapi import register_tortoise
 
 from app.api import ping
+from app.db import init_db
+
+
+log = logging.getLogger("uvicorn")
 
 
 def create_app() -> FastAPI:
     app = FastAPI()
-
-    register_tortoise(
-        app,
-        db_url=os.environ.get("DATABASE_URL"),
-        modules={"models": ["app.models.tortoise"]},
-        generate_schemas=False,
-        add_exception_handlers=True,
-    )
-
     app.include_router(ping.router)
-
     return app
 
 
 app = create_app()
+
+
+@app.on_event("startup")
+async def startup_event():
+    log.info("Starting up...")
+    init_db(app)
+
+
+@app.on_event("shutdown")
+async def shutdown_event():
+    log.info("Shutting down...")
