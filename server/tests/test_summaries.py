4d7765ac5a90fb83ba3355945d4acc47d7550cb6
Den Kasyanov <den@den.ai>
2024-09-12 17:25:40 +0400

    Test creating summary with empty payload

--- server/tests/test_summaries.py
+++ server/tests/test_summaries.py
@@ -3,3 +3,21 @@ def test_create_summary(test_app_with_db):
 
     assert response.status_code == 201
     assert response.json()["url"] == "https://foo.bar"
+
+
+def test_create_summary_with_invalid_json(test_app_with_db):
+    response = test_app_with_db.post("/summaries/", json={})
+
+    assert response.status_code == 422
+
+    print(response.json())
+    assert response.json() == {
+        "detail": [
+            {
+                "input": {},
+                "loc": ["body", "url"],
+                "msg": "Field required",
+                "type": "missing",
+            }
+        ]
+    }
