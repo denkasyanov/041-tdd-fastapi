4147a46686c0a6120b3c7d4b39b3d3d1aae2c9f1
Den Kasyanov <den@den.ai>
2024-09-16 02:41:28 +0400

    Add explicit downloading of tokenizer

--- server/app/summarizer.py
+++ server/app/summarizer.py
@@ -1,3 +1,4 @@
+import nltk
 from newspaper import Article
 
 from app.models.tortoise import TextSummary
@@ -7,6 +8,12 @@ async def generate_summary(summary_id: int, url: str) -> str:
     article = Article(url)
     article.download()
     article.parse()
-    article.nlp()
+
+    try:
+        nltk.data.find("tokenizers/punkt_tab")
+    except LookupError:
+        nltk.download("punkt_tab")
+    finally:
+        article.nlp()
 
     await TextSummary.filter(id=summary_id).update(summary=article.summary)
